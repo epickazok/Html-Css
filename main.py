c3759097cd394b9bd7bacce91d0628086158eeff
Zerrouki kazok <djamel.moho@gmail.com>
2021-02-20 20:26:35 +0100

    new progress

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-import json
-json.dump()
+class Solution:
+    def longestCommonPrefix(self, strs):
+        strs = [str]
+    
+        if len(strs) == 0:
+            return ''
+        res = ''
+        strs = sorted(strs)
+        for i in strs[0]:
+            if strs[-1].startswith(res+i):
+                res += i
+            else:
+                break
+        return res
+
+lonestCommonPrefix(strs['flow', 'flow', 'bruh'])
